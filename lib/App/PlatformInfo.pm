fe80f550f3ec203159a0aa4fdc4984ebacf2c05f
perlancar (@pc-home) <perlancar@gmail.com>
2016-04-10 12:22:36 +0700

    Initial commit

---
+++ lib/App/PlatformInfo.pm
@@ -0,0 +1,13 @@
+package App::PlatformInfo;
+
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Packed platform-info script
+
+=head1 DESCRIPTION
+
+This distribution provides a C<platform-info> which is packed with its
+dependencies so it can run with just perl and core modules.
